47e9b44c6c2113174a3450b7b6301f86bc645ebb
kpla001 <kpla001@fiu.edu>
2022-01-25 12:15:08 -0500

    (prod): begin logic for calculation

--- components/CalculatorOutput/CalculatorOutput.jsx
+++ components/CalculatorOutput/CalculatorOutput.jsx
@@ -2,19 +2,41 @@ import { useState, useEffect } from 'react';
 import styles from './CalculatorOutput.module.css';
 
 export default function CalculatorOutput({ inputData }) {
-  //   console.log(inputData);
-
   const [repaymentSum, setRepaymentSum] = useState(0);
+  const [interestSum, setInterestSum] = useState(0);
+  const [totalNumberOfPayments, setTotalNumberOfPayments] = useState(0);
+
+  //   const interval = {
+  //     daily: 365,
+  //     weekly: 52,
+  //     monthly: 12,
+  //   };
 
   useEffect(() => {
     if (!!inputData) {
       const { startDate, loanAmount, installmentInterval, installmentAmount, interestRate } =
         inputData;
+
+      if (installmentInterval === 'daily') {
+        setTotalNumberOfPayments(
+          Math.ceil((loanAmount + loanAmount * interestRate) / installmentAmount) * 22,
+        );
+      }
+      if (installmentInterval === 'weekly') {
+        setTotalNumberOfPayments(
+          Math.ceil((loanAmount + loanAmount * interestRate) / installmentAmount) * 4,
+        );
+      }
+      if (installmentInterval === 'monthly') {
+        setTotalNumberOfPayments(
+          Math.ceil((loanAmount + loanAmount * interestRate) / installmentAmount),
+        );
+      }
+
+      setInterestSum(loanAmount * interestRate);
       setRepaymentSum(loanAmount + loanAmount * interestRate);
     }
   }, [inputData]);
-
-  //   console.log({ startDate, loanAmount, installmentInterval, installmentAmount, interestRate });
 
   return <div></div>;
 }
